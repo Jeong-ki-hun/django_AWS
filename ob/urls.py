4a3fe8137bf697e02b61daa5be155294a1c33b36
Jeong-ki-hun <jys777999@gmail.com>
2022-11-22 21:22:42 +0900

    5

--- ob/urls.py
+++ ob/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path('map/',views.map, name='map'),
+    path('',views.map, name='map'),
     path('shop/congestion/', views.index, name='index'),
     path('shop/', views.shop_list, name='shop_list'),
     path('shop/dash/',views.Dashborad, name='dash')
