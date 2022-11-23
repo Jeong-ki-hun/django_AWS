9977ffe54238c941ee1644141150f5cc446f0fc2
Jeong-ki-hun <jys777999@gmail.com>
2022-11-23 18:31:59 +0900

    23

--- object/settings.py
+++ object/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'ob.apps.ObConfig',
+    'ob',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
